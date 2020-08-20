bc1f7c84861933e2614718b5a5984e56d444644a
Phillip Choi <jaeja-phil@gmail.com>
2020-08-21 01:01:05 +0900

    final refactoring complete

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,8 @@
 import { MatchReader } from './MatchReader';
-import { CsvFileReader } from './CsvFileReader';
-import { MatchResult } from './MatchResult';
+import { Summary } from './Summary';
 
-// create an object that satisfies the datareader interface
-const csvFileReader = new CsvFileReader('football.csv');
-// create an in stance of matchreader and pass in something satisfiying the datareader interface
-const matchReader = new MatchReader(csvFileReader);
+const matchReader = MatchReader.fromCsv('football.csv');
+const summary = Summary.winsAnalysisWithHtmlReport('Man United');
+
 matchReader.load();
-
-let manUnitedWins = 0;
-
-for (let match of matchReader.matches) {
-  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) manUnitedWins++;
-  else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) manUnitedWins++;
-}
-
-console.log(manUnitedWins);
+summary.buildAndPrintReport(matchReader.matches);
